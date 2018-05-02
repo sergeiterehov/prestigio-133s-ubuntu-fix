6b88be128bd74eb68d3c3426f04f9dd60c71475e
Sergei Terehov <isergeiterehov@gmaail.com>
2018-05-02 15:28:13 +0300

    add real address

--- fix.sh
+++ fix.sh
@@ -6,9 +6,9 @@ sudo apt update
 sudo apt install -y curl linux-generic rtl8723bu-dkms
 
 # Add main fix script
-curl -L https://git/sh/fix-me.sh > /bin/fix-me
+curl -L https://raw.githubusercontent.com/sergeiterehov/prestigio-133s-ubuntu-fix/master/sh/fix-me.sh > /bin/fix-me
 chmod a+x /bin/fix-me
 
 # Add fix script on sleep
-curl -L https://git/sh/fix-me-sleep.sh > /lib/systemd/system-sleep/fix-me-sleep
+curl -L https://raw.githubusercontent.com/sergeiterehov/prestigio-133s-ubuntu-fix/master/sh/fix-me-sleep.sh > /lib/systemd/system-sleep/fix-me-sleep
 chmod a+x /lib/systemd/system-sleep/fix-me-sleep
